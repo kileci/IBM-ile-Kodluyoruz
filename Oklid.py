4a98fc526184fd9511f949a4ca2125fadab2860c
kileci <119419998+kileci@users.noreply.github.com>
2024-11-16 15:23:46 +0300

    Create Oklid.py

---
+++ Oklid.py
@@ -0,0 +1,20 @@
+import math
+
+
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+
+
+points = [(1, 2), (4, 6), (5, 2), (8, 3)]
+
+
+distances = []
+
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):  
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+print("Minimum Öklid Mesafesi:", min_distance)
